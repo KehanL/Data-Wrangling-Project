b0fab17df8e0cb58eeadb4fe5d9219f97a68558b
Kehan Li <stevenlikehan@gmail.com>
2020-05-04 13:34:13 -0400

    Finish the whole project with report

---
+++ keys.R
@@ -0,0 +1,6 @@
+token <- create_token(
+  app = "MLB search",
+  consumer_key = "tov89vja0cKNmma5vtNKLkWlH",
+  consumer_secret = "RPRgcSP8B5kaPUSG6y8ptkJIPmOFFY714C7fRgK6kQt11AXxIB",
+  access_token = "803131058-YJHcJO0Xorur3lOzRvOwqLTDz64ftMsSBvYV5GTf",
+  access_secret = "sTEAaSl2lkavRTXwPV7brUiPAmrtyMLOPIqBrk4nwVtIM")
